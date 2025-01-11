d6de8de8c25ca3a0344472c6ce2627a15881cefe
MananSharma12 <manan.sharma1205@gmail.com>
2025-01-11 21:54:41 +0530

    cleanup: remove shadcn
    - Removing Shadcn to use nuxt ui in future instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,22 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@nuxtjs/color-mode',
     '@pinia/nuxt'
   ],
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
-  },
   typescript: {
     typeCheck: true,
     strict: true,
